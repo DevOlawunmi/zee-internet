e73aa2288032989ebb3886768d0b6561d8edc28d
Ola <olawunmi263@yahoo.com>
2020-03-21 03:38:44 +0000

    Commit initial framework

---
+++ src/main/java/co/uk/theinternet/App.java
@@ -0,0 +1,13 @@
+package co.uk.theinternet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
